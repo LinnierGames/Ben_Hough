1e5682d961a1e39a2f4e2e5fd9df337b12f484b0
Erick <esericksanc@gmail.com>
2015-04-29 17:46:59 -0700

    Terminal > rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429230458) do
+ActiveRecord::Schema.define(version: 20150429233631) do
 
   create_table "blog_comments", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150429230458) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "faqs", force: :cascade do |t|
+    t.string   "question"
+    t.string   "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
